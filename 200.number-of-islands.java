0a357eb83f6f7f40f47749869e5fc247391a788d
naild <dinglekang@gmail.com>
2019-05-14 22:36:35 +0800

    200.number-of-islands

---
+++ 200.number-of-islands.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=200 lang=java
+ *
+ * [200] Number of Islands
+ */
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+        int nums = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                char val = grid[i][j];
+                if (val == '1') {
+                    nums++;
+                    //dfs
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return nums;
+    }
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length 
+            || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
+
