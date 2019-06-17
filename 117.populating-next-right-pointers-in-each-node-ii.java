474538db358884acc712d9cde6e1ecfde4dcd6bf
naild <dinglekang@gmail.com>
2019-06-17 22:36:46 +0800

    117.populating-next-right-pointers-in-each-node-ii

---
+++ 117.populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,54 @@
+import java.util.*;
+/*
+ * @lc app=leetcode id=117 lang=java
+ *
+ * [117] Populating Next Right Pointers in Each Node II
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _left,Node _right,Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    List<Stack<Node>> mem = new ArrayList<>();
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        dfs(root, 0);
+        return root;
+    }
+
+    private void dfs(Node root, int level) {
+        if (root == null) {
+            return;
+        }
+        Stack<Node> stack = null;
+        if (mem.size() == level) {
+            stack = new Stack<>();
+            stack.add(root);
+            mem.add(stack);
+        } else {
+            stack = mem.get(level);
+            Node tmp = stack.pop();
+            tmp.next = root;
+            stack.add(root);
+        }
+        dfs(root.left, level + 1);
+        dfs(root.right, level + 1);
+    }
+}
+
