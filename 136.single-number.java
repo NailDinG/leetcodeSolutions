fa3ba7acbce8242d4020b76b4ac78f936d9af237
naild <dinglekang@gmail.com>
2019-08-07 23:39:12 +0800

    136.single-number

---
+++ 136.single-number.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=136 lang=java
+ *
+ * [136] Single Number
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+
+        for (int i = 1; i < nums.length; i ++) {
+            res = res ^ nums[i];
+        }
+
+        return res;
+    }
+}
+
